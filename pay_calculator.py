6a1bb9ec4c29d19a6b574834632fae770b791787
Joakim Gustafsson <jocke.gustafsson@gmail.com>
2022-09-16 21:27:33 +0200

    changed pay function

--- pay_calculator.py
+++ pay_calculator.py
@@ -1,5 +1,15 @@
+from cgitb import small
 import datetime as dt
-from schedule_generator import SweHolidays
+from sqlite3 import DataError
+import holidays
+from datetime import date
+from dateutil.easter import easter
+from dateutil.relativedelta import relativedelta as rd, FR, SA
+
+from holidays.constants import JAN, MAR, MAY, JUN, OCT, DEC
+from holidays.constants import MON, THU, FRI, SAT, SUN
+from holidays.holiday_base import HolidayBase
+# from schedule_generator import SweHolidays
 
 # A tuple of the shift pattern: strings to be associated with dates
 shifts = ('Fi', 'F', 'E', 'E', 'N', 'Na', 'E',
@@ -9,62 +19,165 @@ shifts = ('Fi', 'F', 'E', 'E', 'N', 'Na', 'E',
           'N', 'N', 'L', 'L', 'L', 'L', 'L')
 
 
-holidays_se = SweHolidays(include_sundays=False)
+# holidays_se = SweHolidays(include_sundays=False)
+
+
+class BigHolidays(HolidayBase):
+    def __init__(self, **kwargs):
+        self.country = "SE"
+        HolidayBase.__init__(self, **kwargs)
+
+    def _populate(self, year):
+
+        # ========= Static holidays =========
+        self[date(year, JAN, 1)] = "Nyårsdagen"
+
+        # self[date(year, JAN, 6)] = "Trettondedag jul"
+
+        # Source: https://sv.wikipedia.org/wiki/F%C3%B6rsta_maj
+        if year >= 1939:
+            self[date(year, MAY, 1)] = "Första maj"
+
+        # Source: https://sv.wikipedia.org/wiki/Sveriges_nationaldag
+        if year >= 2005:
+            self[date(year, JUN, 6)] = "Sveriges nationaldag"
+
+        self[date(year, DEC, 24)] = "Julafton"
+        self[date(year, DEC, 25)] = "Juldagen"
+        self[date(year, DEC, 26)] = "Annandag jul"
+        self[date(year, DEC, 31)] = "Nyårsafton"
+
+        # ========= Moving holidays =========
+        e = easter(year)
+        maundy_thursday = e - rd(days=3)
+        good_friday = e - rd(days=2)
+        easter_saturday = e - rd(days=1)
+        resurrection_sunday = e
+        easter_monday = e + rd(days=1)
+        # ascension_thursday = e + rd(days=39)
+        pentecost = e + rd(days=49)
+        pentecost_day_two = e + rd(days=50)
+
+        assert maundy_thursday.weekday() == THU
+        assert good_friday.weekday() == FRI
+        assert easter_saturday.weekday() == SAT
+        assert resurrection_sunday.weekday() == SUN
+        assert easter_monday.weekday() == MON
+        # assert ascension_thursday.weekday() == THU
+        assert pentecost.weekday() == SUN
+        assert pentecost_day_two.weekday() == MON
+
+        self[good_friday] = "Långfredagen"
+        self[resurrection_sunday] = "Påskdagen"
+        self[easter_monday] = "Annandag påsk"
+        # self[ascension_thursday] = "Kristi himmelsfärdsdag"
+        self[pentecost] = "Pingstdagen"
+        if year <= 2004:
+            self[pentecost_day_two] = "Annandag pingst"
+
+        # Midsummer evening. Friday between June 19th and June 25th
+        self[date(year, JUN, 19) + rd(weekday=FR)] = "Midsommarafton"
+
+        # Midsummer day. Saturday between June 20th and June 26th
+        if year >= 1953:
+            self[date(year, JUN, 20) + rd(weekday=SA)] = "Midsommardagen"
+        else:
+            self[date(year, JUN, 24)] = "Midsommardagen"
+            # All saints day. Friday between October 31th and November 6th
+        # self[date(year, OCT, 31) + rd(weekday=SA)] = "Alla helgons dag"
+
+        if year <= 1953:
+            self[date(year, MAR, 25)] = "Jungfru Marie bebådelsedag"
+
+        # e = easter(year)
+        easter_saturday = e - rd(days=1)
+        pentecost_evening = e + rd(days=48)
+
+        self[easter_saturday] = "Påskafton"
+        self[pentecost_evening] = "Pingstafton"
+
+
+class SmallHolidays(HolidayBase):
+    def __init__(self, **kwargs):
+        self.country = "SE"
+        HolidayBase.__init__(self, **kwargs)
+
+    def _populate(self, year):
+
+        # ========= Static holidays =========
+        self[date(year, JAN, 6)] = "Trettondedag jul"
+
+        # ========= Moving holidays =========
+        e = easter(year)
+        ascension_thursday = e + rd(days=39)
+
+        assert ascension_thursday.weekday() == THU
+
+        self[ascension_thursday] = "Kristi himmelsfärdsdag"
+
+        self[date(year, OCT, 31) + rd(weekday=SA)] = "Alla helgons dag"
+
+
+holidays_se = BigHolidays()
+small_holidays_se = SmallHolidays()
 
 
 def pay(base_pay, date, shift_day):
+    ob1 = 27.44
+    ob2 = 34.91
+    ob3 = 76.80
+    ob4 = 170.82
+
     if shift_day == 'L':
         return 0
     elif shift_day == 'Fi':
-        if date not in holidays_se and date - dt.timedelta(days=1) not in holidays_se:
-            return (base_pay / 270) * 0.25
-        elif date not in holidays_se and date - dt.timedelta(days=1) in holidays_se:
-            return ((base_pay / 270) + (base_pay / 110)) * 0.25
-        elif date in holidays_se and date - dt.timedelta(days=1) not in holidays_se:
-            return ((base_pay / 270) * 8.25) + ((base_pay / 110) * 8)
+        if date in holidays_se:
+            return 0.75 * ob2 + 9.75 * ob4
+        elif date in small_holidays_se:
+            return 0.75 * ob2 + 9.75 * ob3
         else:
-            return ((base_pay / 270) + (base_pay / 110)) * 8.25
+            return 0.75 * ob2
     elif shift_day == 'F':
-        if date not in holidays_se and date - dt.timedelta(days=1) not in holidays_se and date.weekday() not in range(5, 7):
-            return (base_pay / 600) * 0.25
-        elif date not in holidays_se and date - dt.timedelta(days=1) in holidays_se:
-            return ((base_pay / 270) + (base_pay / 110)) * 0.25
-        elif date in holidays_se and date - dt.timedelta(days=1) not in holidays_se and date.weekday() not in range(5, 7):
-            return ((base_pay / 270) * 8) + ((base_pay / 110) * 8) + ((base_pay / 600) * 0.25)
-        elif date in holidays_se and date - dt.timedelta(days=1) in holidays_se:
-            return ((base_pay / 270) + (base_pay / 110)) * 8.25
+        if date in holidays_se:
+            return 0.75 * ob2 + 8.25 * ob4
+        elif date in small_holidays_se:
+            return 0.75 * ob2 + 8.25 * ob3
         elif date.weekday() in range(5, 7):
-            return (base_pay / 270) * 8.25
+            return 0.75 * ob2 + 8.25 * ob3
+        else:
+            return 0.75 * ob2
     elif shift_day == 'D':
-        if date in holidays_se and date - dt.timedelta(days=1) in holidays_se:
-            return ((base_pay / 270) * 12.25) + ((base_pay / 110) * 12.25)
-        elif date in holidays_se and date - dt.timedelta(days=1) not in holidays_se:
-            return ((base_pay / 270) * 12.25) + ((base_pay / 110) * 12)
-        elif date not in holidays_se and date - dt.timedelta(days=1) in holidays_se:
-            return ((base_pay / 270) * 12.25) + ((base_pay / 110) * 0.25)
-        elif date not in holidays_se and date - dt.timedelta(days=1) not in holidays_se:
-            return (base_pay / 270) * 12.25
+        if date in holidays_se:
+            return 0.75 * ob2 + 1.5 * ob1 + 12.25 * ob4
+        elif date in small_holidays_se:
+            return 0.75 * ob2 + 1.5 * ob1 + 12.25 * ob3
+        else:
+            return 0.75 * ob2 + 1.5 * ob1 + 12.25 * ob3
     elif shift_day == 'E':
-        if date not in holidays_se and date.weekday() not in range(4, 7):
-            return (base_pay / 600) * 5
-        elif date in holidays_se:
-            return ((base_pay / 270) * 8.25) + ((base_pay / 110) * 8.25)
-        elif date.weekday() in range(5, 7):
-            return (base_pay / 270) * 8.25
-        elif date.weekday() == 4:
-            return (base_pay / 270) * 5
+        if date in holidays_se:
+            return 5.5 * ob1 + 8.25 * ob4
+        elif date in small_holidays_se:
+            return 5.5 * ob1 + 8.25 * ob3
+        elif date.weekday() == 6:
+            return 5.5 * ob1 + 8.25 * ob3
+        else:
+            return 5.5 * ob1
     elif shift_day == 'N':
-        if date in holidays_se:
-            return ((base_pay / 270) * 8.25) + ((base_pay / 110) * 8.25)
-        elif date.weekday() in range(4, 7):
-            return (base_pay / 270) * 8.25
+        if date + dt.timedelta(days=1) in holidays_se:
+            return 0.75 * ob1 + 7.5 * ob2 + 8.25 * ob4
+        elif date + dt.timedelta(days=1) in small_holidays_se:
+            return 0.75 * ob1 + 7.5 * ob2 + 7.5 * ob3
+        elif date.weekday() == 4:
+            return 0.75 * ob1 + 7.5 * ob2 + 7.5 * ob3
+        elif date.weekday() == 6:
+            return 0.75 * ob1 + 7.5 * ob2 + 0.75 * ob3
         else:
-            return (base_pay / 600) * 8.25
+            return 0.75 * ob1 + 7.5 * ob2
     elif shift_day == 'Na':
-        if date in holidays_se:
-            return ((base_pay / 270) * 12.25) + ((base_pay / 110) * 12.25)
+        if date + dt.timedelta(days=1) in holidays_se:
+            return 4.75 * ob1 + 7.5 * ob2 + 12.25 * ob4
         else:
-            return 4.75 * 27.44 + 12.25 * 76.80 + 7.5 * 34.91
+            return 4.75 * ob1 + 7.5 * ob2 + 12.25 * ob3
 
 
 def shift_day(date, shift):
